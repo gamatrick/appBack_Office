4a49fbec12c030ecbdc7101cc103a98cb9575ea3
gamatrick <val@laflyfamily.com>
2026-01-15 12:29:42 +0100

    add userEntity

---
+++ backend/src/main/java/com/projet/entities.java
@@ -0,0 +1,19 @@
+package com.projet.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data @NoArgsConstructor @AllArgsConstructor
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    private String roles; // On stockera "ADMIN" ou "USER"
+    private boolean enabled = true;
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
