c9527df70cd6aa57bf9b1c7df7c3ea7d1d5fe866
gamatrick <val@laflyfamily.com>
2026-01-15 10:14:20 +0100

    add back-end

---
+++ backend/src/main/java/com/projet/backend/AppBackOfficeApplication.java
@@ -0,0 +1,13 @@
+package com.projet.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBackOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppBackOfficeApplication.class, args);
+	}
+
+}
